9afe5871cab8bc120c10c846f4ce59c6c124e652
Salvo Cirinà <salvocia90@gmail.com>
2014-11-02 20:25:48 +0100

    Project creation

---
+++ Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(enemy.tag.ToString());
+	}
+
+	void onCollisionEnter(GameObject enemy){
+
+		Debug.Log (enemy.tag.ToString);
+	}
+}
